11d1e25321afa5ec580a57bf6f4e8f0c75889ec3
DarkCurut08 <42291700+DarkCurut08@users.noreply.github.com>
2020-04-26 04:51:38 +0700

    Add files via upload

---
+++ Moreno77.sh
@@ -0,0 +1,51 @@
+clear
+echo "\n\n\n\033[1;32m      +---------[ Tools by Moreno77 ]----------+"
+python .py
+sleep 1
+echo " \033[1;32m(\033[1;36m1\033[1;32m) Hack Instagram"
+echo " \033[1;32m(\033[1;36m2\033[1;32m) Hack Facebook"
+echo " \033[1;32m(\033[1;36m3\033[1;32m) Multi Hack Instagram"
+echo " \033[1;32m(\033[1;36m4\033[1;32m) Multi Hack Facebook"
+echo " \033[1;32m(\033[1;36m5\033[1;32m) Auto Visitor Blog"
+echo " \033[1;32m(\033[1;36m6\033[1;32m) Mail Bomber"
+echo " \033[1;32m(\033[1;36m00\033[1;32m)\033[1;31m Exit"
+echo -n " \033[1;33m[*] Input =>\033[1;37m "
+read pil
+case $pil in
+1) cd data
+python 1.py
+exit
+;;
+2) cd data
+python2 2.py
+exit
+;;
+3) cd data
+bash 3.sh
+exit
+;;
+4) cd data
+python2 4.py
+exit
+;;
+5) cd data
+php 6.php
+exit
+;;
+6) cd data
+echo "\033[1;32m "
+python2 5.py
+exit
+;;
+00) echo "Autor : Moreno77" | lolcat
+echo "WhatsApp : +62 831-1361-1267" | lolcat
+echo "Instagram : https://instagram.com/rdh05._" | lolcat
+echo "Channel Youtube : youtube.com/c/DarkCurut08" | lolcat
+figlet -f slant "Moreno77" | lolcat
+echo '[!] Selamat Bertemu Lain Waktu...' | lolcat
+exit
+;;
+esac
+done
+done
+
---
+++ install.sh
@@ -0,0 +1,33 @@
+clear
+pkg update
+clear
+pkg upgrade
+clear
+pkg install python
+clear
+pkg install python2
+clear
+pip2 install requests
+pip install requests
+clear
+pip2 install mechanize
+clear
+pkg install figlet
+clear
+pip2 install lolcat
+clear
+pkg install bash
+clear
+pkg install php
+clear
+cd data
+clear
+apt install jq
+clear
+pkg install jq
+clear
+pkg install curl
+clear
+pkg install ruby
+clear
+exit
